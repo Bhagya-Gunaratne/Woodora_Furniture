d896431cc254845ccaf8a35e0b3bde433398483a
Bhagya-Gunaratne <10898473@students.plymouth.ac.uk>
2025-05-09 05:43:33 +0100

    Product Model Class Created

---
+++ src/Product.java
@@ -0,0 +1,38 @@
+
+public class Product {
+    static String currentId;
+
+    private String  id;
+    private String productName;
+    private String productCategory;
+    private String productImageName;
+    private String productPrice;
+    private String productDescription;
+    private String productMaterial;
+    private String productColor;
+
+    public String getProductName() {
+        return productName;
+    }
+    public String getProductCategory() {
+        return productCategory;
+    }
+    public String getProductImageName() {
+        return productImageName;
+    }
+    public String getProductPrice() {
+        return productPrice;
+    }
+    public String getProductDescription() {
+        return productDescription;
+    }
+    public String getProductMaterial(){
+        return productMaterial;
+    }
+    public String getProductColor(){
+        return productColor;
+    }
+    public String getId(){
+        return id;
+    }
+}
